6c9a61da2334a8af1055e416e165d2904e0a40a0
Oleg Kryuchikhin <adenos90@gmail.com>
2025-06-02 18:39:55 +0300

    Add PUT/PUTCH and DEL solutions

--- cats/views.py
+++ cats/views.py
@@ -6,6 +6,9 @@ from .models import Cat
 from .serializers import CatSerializer
 
 
+# View-функция cat_list() будет обрабатывать только запросы GET и POST,
+# запросы других типов будут отклонены,
+# так что в теле функции их можно не обрабатывать
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
@@ -17,3 +20,32 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, pk):
+    cat = Cat.objects.get(id=pk)
+    # if request.method == 'PUT':
+    #     serializer = CatSerializer(cat, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'PATCH' or request.method == 'PUT':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(f'{cat.name} deleted', status=status.HTTP_204_NO_CONTENT)
+    serializer = CatSerializer(cat)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def hello(request):
+    if request.method == 'POST':
+        return Response({'message': 'Получены данные', 'data': request.data})
+    return Response({'message': 'Это был GET-запрос!'})
