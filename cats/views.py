1aa2e9491d24a74745048843064e543bf4e87956
Oleg Kryuchikhin <adenos90@gmail.com>
2025-06-04 14:55:45 +0300

    AND kittygramm

--- cats/views.py
+++ cats/views.py
@@ -1,11 +1,14 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import viewsets
 
 from .models import Cat
 from .serializers import CatSerializer
 
-
+''' ФУНЦИИ view
 # View-функция cat_list() будет обрабатывать только запросы GET и POST,
 # запросы других типов будут отклонены,
 # так что в теле функции их можно не обрабатывать
@@ -49,3 +52,66 @@ def hello(request):
     if request.method == 'POST':
         return Response({'message': 'Получены данные', 'data': request.data})
     return Response({'message': 'Это был GET-запрос!'})
+
+'''
+
+''' Низкоуровневые view-классы в DRF
+class APICat(APIView):
+
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class APICatDetail(APIView):
+
+    def get(self, request, pk):
+        cat = Cat.objects.get(id=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        cat = Cat.objects.get(id=pk)
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        cat = Cat.objects.get(id=pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        cat = Cat.objects.get(id=pk)
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
+
+''' Высокоуровневые view-классы в DRF, дженерики
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+'''
+
+
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,11 +1,28 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 
-from cats.views import cat_list, hello, cat_detail
+# from cats.views import cat_list, hello, cat_detail ФУНЦИИ view
+# from cats.views import APICat, APICatDetail  Низкоуровневые view-классы в DRF
+# from cats.views import CatList, CatDetail  Высокоуровневые view-классы в DRF, дженерики
+from cats.views import CatViewSet
+
+# Создаётся роутер
+router = SimpleRouter()
+# Вызываем метод .register с нужными параметрами
+router.register('cats', CatViewSet)
+# В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# например
+# router.register('owners', OwnerViewSet)
+# Но нам это пока не нужно
+
 
 urlpatterns = [
-   path('cats/<int:pk>/', cat_detail),
-   path('cats/', cat_list),
-   path('hello/', hello),
+    # path('cats/<int:pk>/', cat_detail),   ФУНЦИИ view
+    # path('cats/', cat_list),  ФУНЦИИ view
+    # path('hello/', hello),  ФУНЦИИ view
+    # path('cats/<int:pk>/', APICatDetail.as_view()),  Низкоуровневые view-классы в DRF
+    # path('cats/', APICat.as_view())  Низкоуровневые view-классы в DRF
+    # path('cats/<int:pk>/', CatDetail.as_view()),  Высокоуровневые view-классы в DRF, дженерики
+    # path('cats/', CatList.as_view())  Высокоуровневые view-классы в DRF, дженерики
+    path('', include(router.urls)),
 ]
-
-
